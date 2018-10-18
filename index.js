437e2b5243ffa356f5e4b7cf016c80a36558cf94
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-18 19:07:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function happyHolidays()
 
 function happyHolidaysTo(name) {
 
-  return '"Happy holidays, ${name}!"'
+  return `"Happy holidays, ${name}!"`
 }
 function happyHolidayTo(holiday,name) {
-  return ''
+  return `Happy ${holiday}, ${name}!`
 }
