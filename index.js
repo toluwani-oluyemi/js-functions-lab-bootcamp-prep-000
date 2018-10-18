bd38aeb2fb71165b66bca6fc10c2d22508a8dff5
Toluwani Oluyemi <toluwani.oluyemi@gmail.com>
2018-10-18 18:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // write your code below!
 function happyHolidays(String)
 {
- return "Happy Holidays!"; 
+ return String. "Happy Holidays!"; 
 }
 function happyHolidaysTo()${name}{
  return 'Happy holidays, ${name}!'
